92ecf9f5b03e4b45e6322af00967fd80ccb038e5
Anugrah-Chandran <anugrahchandran2028@cs.sjcetpalai.ac.in>
2024-11-29 15:39:21 +0530

    Add files via upload

---
+++ factorial_function.py
@@ -0,0 +1,12 @@
+'''Author: Anugrah Chandran
+Date : 29-11-24
+Description : Program to find the factorial of a number using Recursion.'''
+def factorial(num):
+    if num == 0:
+        return 1
+    else:
+        return(num*factorial(num-1))
+
+num = int(input("Enter a number: "))
+factorial(num)
+print(factorial(num))
